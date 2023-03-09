6633b5d8c23f5346efecdd02c8beab7a232bcd0e
peterdavidfagan <peterdavidfagan@gmail.com>
2023-03-09 12:21:02 +0000

    remove resource

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
